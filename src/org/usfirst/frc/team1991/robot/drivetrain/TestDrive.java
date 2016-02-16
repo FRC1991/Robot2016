7e1bacf37505a4dc45d6e261931a998a80bba177
Nexuist <nexuister@gmail.com>
2016-02-16 00:14:55 -0500

    Removed defunct commands and rewrote ReverseDrive (now ChangeDirection)

--- src/org/usfirst/frc/team1991/robot/drivetrain/TestDrive.java
+++ src/org/usfirst/frc/team1991/robot/drivetrain/TestDrive.java
@@ -7,10 +7,10 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
  *
  */
 public class TestDrive extends CommandGroup {
-    
+
     public TestDrive() {
-        addSequential(new AutoDrive(0.5, 0.5, 3));
-        addSequential(new AutoDrive(1, 0, 1.3));
-        addSequential(new AutoDrive(0.6, 0.6, 1));
+        addSequential(new Drive(0.5, 0.5, 3));
+        addSequential(new Drive(1, 0, 1.3));
+        addSequential(new Drive(0.6, 0.6, 1));
     }
 }
