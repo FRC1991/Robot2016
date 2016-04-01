6058235273103f37ebafd5b1dbe4df4d64967b68
1991 Dragons <aakashbalaji.kumar@gmail.com>
2016-04-01 12:45:59 -0400

    4/1/2016
    Removed speed restrictions on drivetrain + lowered manual shooter speed.
    Also moved autonomous code into methods, added distance reckoning, and
    position based decision making.

--- src/autonomous/Autonomous.java
+++ src/autonomous/Autonomous.java
@@ -9,63 +9,76 @@ import src.teleop.DriveStraight;
 import src.teleop.SetDriveBackwards;
 import src.teleop.Shoot;
 
-public class Autonomous extends CommandGroup{
+public class Autonomous extends CommandGroup {
 	private int mode, position;
-	private final double driveTime = 4.2;
-	private final double driveSpeed = 0.2;
+	private final double driveTime = 2.5;
+	private final double driveSpeed = 0.3;
 	
 	public Autonomous() {
 		addSequential(new MoveSystemsToPositions(Robot.Position.IntakeDown, Robot.Position.ShooterStowed));
 		addSequential(new WaitCommand(1));
 		mode = (int)SmartDashboard.getNumber("Autonomous Mode", 0);
 		position = (int)SmartDashboard.getNumber("Autonomous Position", 0);
+		
 		System.out.println("Autonomous started in mode " + mode + ", position " + position);
+		
 		switch(mode) {
-		case 0:
-	    	addSequential(new DriveTime(driveTime, driveSpeed));
-	    	addSequential(new MoveShooterToPosition(Robot.Position.ShooterAutoAim));
+		case 0: System.out.println("Not doing anything.");
 			break;
-	    case 1: 
-	    	addSequential(new DriveTime(driveTime, driveSpeed));
+	    case 1: moveUp();
 	    	break;
-	    case 2:
-//	    	addSequential(new DriveTime(driveTime, driveSpeed));
-//	    	addSequential(new SetDriveBackwards(true));
-//	    	addSequential(new WaitCommand(0.5));
-//	    	addSequential(new DriveTime(driveTime, driveSpeed));
-//	    	addSequential(new SetDriveBackwards(false));
-	    	// Drive and auto aim
-	    	addSequential(new DriveTime(driveTime, driveSpeed));
-	    	addSequential(new MoveShooterToPosition(Robot.Position.ShooterAutoAim));
-	    	addSequential(new WaitCommand(2));
-	    	addSequential(new TurnToAlignWithTarget());
+	    case 2: moveUpNoShoot();
 	    	break;
-	    case 3:
-	    	addSequential(new DriveTime(driveTime, driveSpeed));
-	    	addSequential(new MoveShooterToPosition(Robot.Position.ShooterAutoAim));
-	    	addSequential(new WaitCommand(2));
-	    	addSequential(new TurnToAlignWithTarget());
-	    	addSequential(new WaitCommand(0.3));
-	    	// Check if shot should be taken
-	    	addSequential(new XCommand() {
-	    		private double farthestDistance = 11.5;
-	    		private double lowestDistance = 7;
-				@Override
-				protected void execute() {
-				}
-				
-				protected boolean isFinished() {
-					double distance = SmartDashboard.getNumber("Distance", 100);
-					return (distance > lowestDistance && distance < farthestDistance);
-					
-				}
-	    	});
-	    	addSequential(new MoveShooterToPosition(Robot.Position.ShooterFeed));
-	    	addSequential(new WaitCommand(1));
-	    	addSequential(new Shoot());
+	    case 3: moveUpShoot();
 	    	break;
-	    default: 
+	    default: System.out.println("Not doing anything. ");
 	   		break;
 	    }
+	}
+	
+	private void moveUp(){
+		addSequential(new DriveTime(driveTime, driveSpeed));
+	}
+	
+	private void moveUpNoShoot(){
+		moveUp();
+		//positionStrategy();
+    	addSequential(new MoveShooterToPosition(Robot.Position.ShooterAutoAim));
+    	addSequential(new WaitCommand(2));
+    	addSequential(new TurnToAlignWithTarget());
+    	addSequential(new WaitCommand(1));
+    	addSequential(new DistanceReckon());
+	}
+	
+	private void moveUpShoot(){
+		moveUpNoShoot();
+		addSequential(new WaitCommand(0.5));
+		shoot();
+	}
+	
+	private void shoot(){
+    	addSequential(new MoveShooterToPosition(Robot.Position.ShooterFeed));
+    	addSequential(new WaitCommand(1));
+    	addSequential(new Shoot());
+	}
+	
+	private void positionStrategy(){
+		switch(position){
+		//Low Bar
+		case 1: addSequential(new TurnToYaw(15));
+			break;
+		//A Bit Left
+		case 2: addSequential(new TurnToYaw(10));
+			break;
+		//Middle
+		case 3: System.out.println("No need to turn!");
+			break;
+		//Middle
+		case 4: System.out.println("No need to turn!");
+			break;
+		//A Bit Right
+		case 5: addSequential(new TurnToYaw(-10));
+			break;
+		}
 	}
 }
